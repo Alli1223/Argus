6a0bcdb76781000407b795d2ca7cf9ebfeb8b5cd
Alastair Rayner <alli0@me.com>
2026-09-15 20:22:26 +0100

    Show page errors inside the app shell

--- web/src/app/RouteError.tsx
+++ web/src/app/RouteError.tsx
@@ -2,6 +2,14 @@ import { Anchor, Button, Group } from "@mantine/core";
 import { isRouteErrorResponse, Link, useRouteError } from "react-router";
 import { MessageScreen } from "../components/Screens";
 
+/** A page's code failed to load, usually because Argus was updated after this tab was opened. */
+function isStaleBuild(error: unknown): boolean {
+  return (
+    error instanceof Error &&
+    /dynamically imported module|importing a module script failed/i.test(error.message)
+  );
+}
+
 export function RouteError() {
   const error = useRouteError();
 
@@ -9,14 +17,29 @@ export function RouteError() {
     return <NotFoundPage />;
   }
 
+  const reload = (
+    <Button variant="light" onClick={() => window.location.reload()}>
+      Reload the page
+    </Button>
+  );
+
+  if (isStaleBuild(error)) {
+    return (
+      <MessageScreen title="Argus has been updated" action={reload}>
+        This tab is still running the previous version. Reload it to continue.
+      </MessageScreen>
+    );
+  }
+
   return (
     <MessageScreen
       title="Something went wrong"
       action={
-        <Group>
-          <Button variant="light" onClick={() => window.location.reload()}>
-            Reload the page
-          </Button>
+        <Group gap="md">
+          {reload}
+          <Anchor component={Link} to="/">
+            Go to the overview
+          </Anchor>
         </Group>
       }
     >
